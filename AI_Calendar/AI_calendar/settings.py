c62ada6f846d1f0df12f1566bdd456c8096b3498
Softicles <thinhmnguyen4105@gmail.com>
2025-07-26 00:05:16 -0400

    change database to PostgreSQL

--- AI_Calendar/AI_calendar/settings.py
+++ AI_Calendar/AI_calendar/settings.py
@@ -15,6 +15,7 @@ import os
 from decouple import config
 import logging
 from dotenv import load_dotenv
+from urllib.parse import urlparse, parse_qsl
 
 load_dotenv()
 logging.basicConfig(level=logging.DEBUG)
@@ -120,10 +121,25 @@ WSGI_APPLICATION = "AI_calendar.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# Add these at the top of your settings.py
+import os
+from dotenv import load_dotenv
+from urllib.parse import urlparse, parse_qsl
+
+load_dotenv()
+
+# Replace the DATABASES section of your settings.py with this
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
+        'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
     }
 }
 
